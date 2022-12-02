0df014e10729787927b4596bab2b191cc23c0c5e
labreezy <yourtooslow@gmail.com>
2022-12-01 22:58:21 -0600

    patches shouldn't be an issue anymore

--- PortalGear/MainWindow.xaml.cs
+++ PortalGear/MainWindow.xaml.cs
@@ -11,6 +11,7 @@ using System.Threading.Tasks;
 using Reloaded.Assembler;
 using Reloaded.Memory;
 using Reloaded.Memory.Sources;
+using Reloaded.Memory.Sigscan;
 using SharpHook.Native;
 
 namespace PortalGear
@@ -58,7 +59,7 @@ namespace PortalGear
         private ulong curr_pos_addr;
         private ulong curr_cam_addr;
         private byte[] origbytes_pos_speed = new byte[15];
-        private byte[] origbytes_camera = new byte[10];
+        private byte[] origbytes_camera = new byte[17];
         private Assembler asmblr = new Assembler();
         private ExternalMemory front_mem;
         private bool isAttached = false;
@@ -133,13 +134,30 @@ namespace PortalGear
                 posUpdateTimer = new DispatcherTimer();
                 frontiersProc = proc;
                 front_mem = new ExternalMemory(frontiersProc);
+                var scanner = new Scanner(frontiersProc, frontiersProc.MainModule);
+                pos_inject_off = scanner.FindPattern("0F 58 B3 80 00 00 00").Offset;
+                if (pos_inject_off < 0)
+                {
+                    MessageBox.Show("Position sigscan failed!");
+                    return;
+                }
+
+                pos_inject_off -= 8;
+                cam_inject_off = scanner.FindPattern("48 8B D8 0F 11 00 0F 10 4F").Offset;
+                if (cam_inject_off < 0)
+                {
+                    MessageBox.Show("Camera sigscan failed!");
+                    return;
+                }
+
+                cam_inject_off -= 3;
                 cave1_loc = IntPtr.Add(frontiersProc.MainModule.BaseAddress, cave1_off);
                 cave2_loc = IntPtr.Add(frontiersProc.MainModule.BaseAddress, cave2_off);
                 pos_inject_loc = IntPtr.Add(frontiersProc.MainModule.BaseAddress, pos_inject_off);
                 cam_inject_loc = IntPtr.Add(frontiersProc.MainModule.BaseAddress, cam_inject_off);
                 front_mem.SafeRead<ExternalMemory, byte>((nuint) pos_inject_loc.ToInt64(), out origbytes_pos_speed, 15,
                     false);
-                front_mem.SafeRead<ExternalMemory, byte>((nuint) cam_inject_loc.ToInt64(), out origbytes_camera, 10);
+                front_mem.SafeRead<ExternalMemory, byte>((nuint) cam_inject_loc.ToInt64(), out origbytes_camera, 17);
                 pos_jmp_loc = front_mem.Allocate(128);
                 cam_jmp_loc = front_mem.Allocate(128);
                 addr_info_loc = front_mem.Allocate(128);
@@ -156,7 +174,7 @@ inc rsi");
                         $@"use64
 mov rsi,0x{(ulong) addr_info_loc:X}
 mov [rsi],rbx
-movaps xmm6,xmm7
+movaps xmm6 ,xmm7
 mulps xmm6,[rsp+0x20]
 addps xmm6,[rbx+0x80]
 mov rsi,0x{(pos_inject_loc.ToInt64() + 9):x}
@@ -168,6 +186,7 @@ mov r10,0x{(ulong)cam_jmp_loc:x}
 jmp r10
 pop r10
 nop
+nop
 nop");
                 byte[] cam_hook = asmblr.Assemble($@"use64
 mov r10,0x{(ulong) (addr_info_loc + 8):X}
@@ -176,11 +195,14 @@ mov rcx,rax
 mov rbx,rax
 movups [rax],xmm0
 movups xmm1,[rdi+0x10]
-mov r10,0x{(ulong) (pos_inject_loc.ToInt64() + 10):x}
+movups [rax+0x10],xmm1
+mov r10,0x{(ulong) (cam_inject_loc.ToInt64() + 12):x}
 jmp r10
-"); //+10 is 9 for mov+jmp + an extra 1 for push
+"); //+12 is 9 for mov+jmp + an extra few because yes
                 front_mem.Write<ExternalMemory, byte>(pos_jmp_loc, pos_hook);
                 front_mem.SafeWrite<ExternalMemory, byte>((nuint) pos_inject_loc.ToInt64(), pos_jmpbytes);
+                front_mem.Write<ExternalMemory, byte>(cam_jmp_loc, cam_hook);
+                front_mem.SafeWrite<ExternalMemory, byte>((nuint) cam_inject_loc.ToInt64(), cam_jmpbytes);
                 isAttached = true;
                 attachBtn.Content = "Detach";
                 posUpdateTimer.Interval = new TimeSpan(0, 0, 0, 0, 200);
@@ -194,7 +216,8 @@ jmp r10
             else
             {
                 posUpdateTimer.Stop();
-                front_mem.SafeWrite<ExternalMemory, Byte>((nuint) pos_inject_loc.ToInt64(), origbytes_pos_speed, false);
+                front_mem.SafeWrite<ExternalMemory, byte>((nuint) pos_inject_loc.ToInt64(), origbytes_pos_speed, false);
+                front_mem.SafeWrite<ExternalMemory, byte>((nuint) cam_inject_loc.ToInt64(), origbytes_camera, false);
                 isAttached = false;
                 attachBtn.Content = "Attach";
                 kbHook.Dispose();
@@ -219,7 +242,7 @@ jmp r10
             if (curr_cam_addr != 0)
             {
                 front_mem.Read<Vec3>((nuint) (curr_cam_addr + 0x30), out saved_camera, true);
-                camTextBlock.Text = $"(Camera) X:{saved_camera.x:F2}\nY: {saved_camera.y:F2}\nZ: {saved_pos.z:F2}";
+                
             }
         }
 
